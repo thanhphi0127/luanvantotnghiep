c0977ed44a1801d8c6616cdf733d61344e1dc639
phi111325@student.ctu.edu.vn <phi111325@student.ctu.edu.vn@4f87186c-fda9-ace8-9e71-71bb4c24e17a>
2015-01-15 15:28:56 +0000

    file source

--- source/ThapHaNoi_NguyenThanhPhi/ThapHaNoi_NguyenThanhPhi/ThapHaNoi.xaml.cs
+++ source/ThapHaNoi_NguyenThanhPhi/ThapHaNoi_NguyenThanhPhi/ThapHaNoi.xaml.cs
@@ -17,8 +17,8 @@ using System.Reflection;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Media;
 using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.GamerServices;
 using System.Windows.Threading;
-
 
 
 
@@ -30,6 +30,10 @@ namespace ThapHaNoi_NguyenThanhPhi
         TimeSpan time;
         Sounds sounds = new Sounds();
         Picture[] pics;
+        Picture pic1;
+
+
+        Stack<Picture> stack_pic = new Stack<Picture>();
 
         private DispatcherTimer _timer;
         private DateTime _startTime;
@@ -49,7 +53,6 @@ namespace ThapHaNoi_NguyenThanhPhi
             _timer = new DispatcherTimer();
             _timer.Tick += new EventHandler(TimerTick);
 
-           // pics = new Picture[] { Disk1, pics2, pic3, pic4, pic5, pic6, pic7, pic8, pic8, pic9, pic10};
 
         }
 
@@ -61,7 +64,7 @@ namespace ThapHaNoi_NguyenThanhPhi
         void TimerTick(object sender, EventArgs e)
         {
             var time = DateTime.Now - _startTime;
-            txtThoigian.Text = string.Format(Const.TimeFormat, time.Hours, time.Minutes, time.Seconds);
+            txtThoigian.Text = string.Format("{0:00}:{1:00}:{2:00}", time.Hours, time.Minutes, time.Seconds);
         }
 
         /*********************************************************************************
