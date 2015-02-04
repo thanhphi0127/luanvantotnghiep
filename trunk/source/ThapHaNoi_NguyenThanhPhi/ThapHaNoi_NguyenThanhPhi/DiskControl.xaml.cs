d54bf737733f43ccd1edf9f95489ff0b33231d2e
phi111325@student.ctu.edu.vn <phi111325@student.ctu.edu.vn@4f87186c-fda9-ace8-9e71-71bb4c24e17a>
2015-02-04 05:39:42 +0000

    Them code hoan thanh chuc nang Choi game 60%

---
+++ trunk/source/ThapHaNoi_NguyenThanhPhi/ThapHaNoi_NguyenThanhPhi/DiskControl.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace ThapHaNoi_NguyenThanhPhi
+{
+    public partial class DiskControl : UserControl
+    {
+
+        private TranslateTransform move = new TranslateTransform();
+        private TransformGroup rectangleTransforms = new TransformGroup();
+
+        public DiskControl()
+        {
+            InitializeComponent();
+
+        }
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public static readonly DependencyProperty TextProperty =
+             DependencyProperty.Register("Text", typeof(string), typeof(DiskControl), null);
+
+        public delegate void _Tap(DiskControl disk);
+        public _Tap OnTap;
+
+        private void user_ManipulationDelta(object sender, ManipulationDeltaEventArgs e)
+        {
+
+        }
+
+    }
+}
