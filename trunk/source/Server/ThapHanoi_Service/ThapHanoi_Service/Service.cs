3d3e179b9ae077528dc55ad941eac61031a08614
phi111325@student.ctu.edu.vn <phi111325@student.ctu.edu.vn@4f87186c-fda9-ace8-9e71-71bb4c24e17a>
2015-02-16 12:25:47 +0000

    Them code Source/Choidon/PlayGame_Hanoi4 Chua hoan thanh UI di chuyen.

---
+++ trunk/source/Server/ThapHanoi_Service/ThapHanoi_Service/Service.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThapHanoi_Service
+{
+    public class Thachdau: IService
+    {
+        /// <summary>
+        /// Kiểm tra sự tồn tại của một tài khoản
+        /// </summary>
+        /// <param name="tk">tên tài khoản</param>
+        /// <param name="mk">mật khẩu</param>
+        /// <returns>bool: đúng nếu tài khoản chưa tồn tại trong CSDL và ngược lại</returns>
+        public bool KiemTraTaiKhoan(string tk, string mk)
+        {
+            int count = 0;
+            using (TowerHanoiEntities database = new TowerHanoiEntities())
+            {
+                count = (from x in database.NGUOICHOIs
+                         where String.Compare(tk, x.TAIKHOAN.Trim()) == 0 &&
+                         String.Compare(mk, x.MATKHAU.Trim()) == 0
+                         select x.TAIKHOAN).Count();
+            }
+            return (count == 1) ? true : false;
+        }
+    }
+}
--- trunk/source/ThapHaNoi_NguyenThanhPhi/ThapHaNoi_NguyenThanhPhi/Contants.cs
+++ trunk/source/ThapHaNoi_NguyenThanhPhi/ThapHaNoi_NguyenThanhPhi/Contants.cs
@@ -11,6 +11,7 @@ namespace ThapHaNoi_NguyenThanhPhi
         public const int SpaceDisk = 32;
         public const int LeftCanvas = 40;
         public const int TopDisk = 350;
+        public const int LeftCanvas4 = 12;
         public const string TimeFormat = "{0:00}:{1:00}:{2:00}";
         public const string DefaultCountValue = "Số lần chuyển: 0";
         public const string DefaultTimeValue = "00:00:00";
